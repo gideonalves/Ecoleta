b91015cc1595e96502eef1f9e4b8bf45dcf0e3d1
Amanda <amanda.lrib@gmail.com>
2020-06-06 23:54:34 -0300

    Integração do nunjuck ao html

--- src/server.js
+++ src/server.js
@@ -1,4 +1,30 @@
 const express = require("express")
 const server = express()
 
+//configurar pasta pública
+server.use(express.static("public"))
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views",{
+    express: server,
+    noCache: true
+})
+
+//config de rotas
+server.get("/", (req,res) =>{
+    return res.render("index.html",{
+        title:"one title"
+    })
+})
+
+server.get("/create-point", (req,res) =>{
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req,res) =>{
+    return res.render("search-results.html")
+})
+
 server.listen(3000)
+
+
